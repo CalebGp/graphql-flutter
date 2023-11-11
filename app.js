fcbf6b519dab82b5ade10d31cc33a2bfd81c00e7
calebgp <caleb.gp.dev@gmail.com>
2023-11-11 20:24:05 -0300

    correct

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+// ytits2m
 const express = require("express");
+var { graphqlHTTP } = require("express-graphql");
 
-const app = express();
-const { graphqlHTTP } = require("express-graphql");
 const schema = require("./server/schema/schema");
-
 const testSchema = require("./server/schema/types_schema");
 const mongoose = require("mongoose");
-const dbPath = `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.ytits2m.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`;
-//mongodb+srv://caleb:<password>@graphqlcluster.ytits2m.mongodb.net/?retryWrites=true&w=majority
 
+const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
 
@@ -22,10 +20,19 @@ app.use(
 );
 
 mongoose
-  .connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(
+    `mongodb+srv://caleb:luna1212@graphqlcluster.ytits2m.mongodb.net/GraphQlCluster?retryWrites=true&w=majority
+`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
-    app.listen(port, () => {
-      console.log("Listening on the port 4000");
+    app.listen({ port: port }, () => {
+      console.log(process.env.mongoUserName);
+      //localhost:4000
+      console.log("Listening for requests on my awesome port " + port);
     });
   })
-  .catch((e) => console.log(e));
+  .catch((e) => {
+    console.log(process.env.mongoUserName);
+    return console.log("Error:::" + e);
+  });
